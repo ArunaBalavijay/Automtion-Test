2d32020d1637c4a9859d7727893dffcbbeaaba96
Aruna Balasubramanian <aruna.bala@zip.co>
2020-04-13 16:51:14 +1000

    configuration refactoring

--- src/test/java/framework/utilities/Configuration.java
+++ src/test/java/framework/utilities/Configuration.java
@@ -13,20 +13,22 @@ public class Configuration implements IConfiguration {
 		globalProperties = new Properties();
 		environmentProperties = new Properties();
 		
-		try {
-			globalProperties.load(ResourceHelper.getResourcePathInputStream("properties/global.properties"));
-			String environment = getEnvironment();
-			environmentProperties.load(ResourceHelper.getResourcePathInputStream("properties/" + environment + ".properties"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		// Load global configuration
+		loadConfig(globalProperties, "global.properties");
+		// Get current environment
+		String environment = getEnvironment();
+		// Load environment based configuration
+		loadConfig(environmentProperties, environment + ".properties");
 	}
 	
 	@Override
 	public String get(String propertyName) {
+		// Check environment based configuration first
 		String value = environmentProperties.getProperty(propertyName);
 		
+		// If nothing exists in environment configurations
 		if(value == null) {
+			// Then check global configurations
 			value = globalProperties.getProperty(propertyName);
 		}
 		
@@ -36,5 +38,14 @@ public class Configuration implements IConfiguration {
 	@Override
 	public String getEnvironment() {
 		return get("environment");
+	}
+	
+	private void loadConfig(Properties properties, String propertyFileName) {
+		try {
+			// Load properties from properties file
+			properties.load(ResourceHelper.getResourcePathInputStream("properties/" + propertyFileName));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
